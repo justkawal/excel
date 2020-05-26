daa0b8ba982177cdc70c6d8e67a09f91889f4a69
kawal7415 <kawal741536@gmail.com>
2020-05-26 20:59:36 +0530

    formula process started

--- lib/src/sheet/formula.dart
+++ lib/src/sheet/formula.dart
@@ -1,5 +1,37 @@
 part of excel;
 
 class Formula {
-  int sum() {}
+  dynamic _evaluatedValue;
+  String _formulaString;
+
+  Formula._(dynamic evaluatedValue, String formula) {
+    this._evaluatedValue = evaluatedValue;
+    this._formulaString = formula;
+  }
+
+  static Formula abs(Sheet sheet, CellIndex cellIndex) {
+    _checkCellIndex(sheet, cellIndex);
+    ///// process the values here and do the operations
+    return Formula._('ERROR', '=IsSUM()');
+  }
+
+  static _checkCellIndex(Sheet sheet, CellIndex cellIndex) {
+    if (cellIndex == null ||
+        cellIndex.columnIndex < 0 ||
+        cellIndex.rowIndex < 0) {
+      _damagedExcel(text: 'dirty CellIndex');
+    }
+    sheet._checkMaxCol(cellIndex.columnIndex);
+    sheet._checkMaxRow(cellIndex.rowIndex);
+  }
+
+  /// get evaluated String of Formula
+  get value {
+    return this._evaluatedValue;
+  }
+
+  /// get Formula
+  get formula {
+    return this._formulaString;
+  }
 }
