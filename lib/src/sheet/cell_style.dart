ca147b522836bd7ae884c9f4548d49400b365c35
Kawaljeet <49296873+justkawal@users.noreply.github.com>
2021-04-04 22:04:40 +0530

    cell style

--- lib/src/sheet/cell_style.dart
+++ lib/src/sheet/cell_style.dart
@@ -1,7 +1,8 @@
 part of excel;
 
 /// Styling class for cells
-class CellStyle {
+// ignore: must_be_immutable
+class CellStyle extends Equatable {
   String _fontColorHex, _backgroundColorHex, _fontFamily;
   HorizontalAlign _horizontalAlign;
   VerticalAlign _verticalAlign;
@@ -23,151 +24,119 @@ class CellStyle {
     bool italic = false,
     int rotation = 0,
   }) {
-    this._textWrapping = textWrapping;
+    _textWrapping = textWrapping;
 
-    this._bold = bold ?? false;
+    _bold = bold ?? false;
 
-    this.fontSize = fontSize;
+    fontSize = fontSize;
 
-    this._italic = italic ?? false;
+    _italic = italic ?? false;
 
-    this.fontFamily = fontFamily;
+    fontFamily = fontFamily;
 
-    this._rotation = rotation ?? 0;
+    _rotation = rotation ?? 0;
 
     if (fontColorHex != null) {
-      this._fontColorHex = _isColorAppropriate(fontColorHex);
+      _fontColorHex = _isColorAppropriate(fontColorHex);
     } else {
-      this._fontColorHex = "FF000000";
+      _fontColorHex = "FF000000";
     }
 
     if (backgroundColorHex != null) {
-      this._backgroundColorHex = _isColorAppropriate(backgroundColorHex);
+      _backgroundColorHex = _isColorAppropriate(backgroundColorHex);
     } else {
-      this._backgroundColorHex = "none";
+      _backgroundColorHex = "none";
     }
 
-    this._verticalAlign = verticalAlign ?? VerticalAlign.Bottom;
+    _verticalAlign = verticalAlign ?? VerticalAlign.Bottom;
 
-    this._horizontalAlign = horizontalAlign ?? HorizontalAlign.Left;
+    _horizontalAlign = horizontalAlign ?? HorizontalAlign.Left;
   }
 
-  ///
   ///Get Font Color
-  ///
   String get fontColor {
-    return this._fontColorHex;
+    return _fontColorHex;
   }
 
-  ///
   ///Set Font Color
-  ///
   set fontColor(String fontColorHex) {
     if (fontColorHex != null) {
-      this._fontColorHex = _isColorAppropriate(fontColorHex);
+      _fontColorHex = _isColorAppropriate(fontColorHex);
     } else {
-      this._fontColorHex = "FF000000";
+      _fontColorHex = "FF000000";
     }
   }
 
-  ///
   ///Get Background Color
-  ///
   String get backgroundColor {
-    return this._backgroundColorHex;
+    return _backgroundColorHex;
   }
 
-  ///
   ///Set Background Color
-  ///
   set backgroundColor(String backgroundColorHex) {
     if (backgroundColorHex != null) {
-      this._backgroundColorHex = _isColorAppropriate(backgroundColorHex);
+      _backgroundColorHex = _isColorAppropriate(backgroundColorHex);
     } else {
-      this._backgroundColorHex = "none";
+      _backgroundColorHex = "none";
     }
   }
 
-  ///
   ///Get Horizontal Alignment
-  ///
   HorizontalAlign get horizontalAlignment {
-    return this._horizontalAlign;
+    return _horizontalAlign;
   }
 
-  ///
   ///Set Horizontal Alignment
-  ///
   set horizontalAlignment(HorizontalAlign horizontalAlign) {
-    this._horizontalAlign = horizontalAlign ?? HorizontalAlign.Left;
+    _horizontalAlign = horizontalAlign ?? HorizontalAlign.Left;
   }
 
-  ///
   ///Get Vertical Alignment
-  ///
   VerticalAlign get verticalAlignment {
-    return this._verticalAlign;
+    return _verticalAlign;
   }
 
-  ///
   ///Set Vertical Alignment
-  ///
   set verticalAlignment(VerticalAlign verticalAlign) {
-    this._verticalAlign = verticalAlign ?? VerticalAlign.Bottom;
+    _verticalAlign = verticalAlign ?? VerticalAlign.Bottom;
   }
 
-  ///
   ///`Get Wrapping`
-  ///
   TextWrapping get wrap {
-    return this._textWrapping;
+    return _textWrapping;
   }
 
-  ///
   ///`Set Wrapping`
-  ///
   set wrap(TextWrapping textWrapping) {
-    this._textWrapping = textWrapping;
+    _textWrapping = textWrapping;
   }
 
-  ///
   ///`Get FontFamily`
-  ///
   String get fontFamily {
-    return this._fontFamily;
+    return _fontFamily;
   }
 
-  ///
   ///`Set FontFamily`
-  ///
   set fontFamily(String family) {
-    this._fontFamily = family;
+    _fontFamily = family;
   }
 
-  ///
   ///Get Font Size
-  ///
   int get fontSize {
-    return this._fontSize;
+    return _fontSize;
   }
 
-  ///
   ///Set Font Size
-  ///
   set fontSize(int _font_Size) {
-    this._fontSize = _font_Size;
+    _fontSize = _font_Size;
   }
 
-  ///
   ///Get Rotation
-  ///
   int get rotation {
-    return this._rotation;
+    return _rotation;
   }
 
-  ///
   ///Rotation varies from [90 to -90]
-  ///
   set rotation(int _rotate) {
     if (_rotate > 90 || _rotate < -90) {
       _rotate = 0;
@@ -178,70 +147,51 @@ class CellStyle {
       /// -(_rotate) + 90
       _rotate = -(_rotate) + 90;
     }
-    this._rotation = _rotate;
+    _rotation = _rotate;
   }
 
-  ///
   ///Get `Underline`
-  ///
   Underline get underline {
-    return this._underline;
+    return _underline;
   }
 
-  ///
-  ///set `Underline`
-  ///
+  ///Set `Underline`
   set underline(Underline underline_) {
-    this._underline = underline_ ?? Underline.None;
+    _underline = underline_ ?? Underline.None;
   }
 
-  ///
   ///Get `Bold`
-  ///
   bool get isBold {
-    return this._bold;
+    return _bold;
   }
 
-  ///
   ///Set `Bold`
-  ///
   set isBold(bool bold) {
-    this._bold = bold ?? false;
+    _bold = bold ?? false;
   }
 
-  ///
   ///Get `Italic`
-  ///
   bool get isItalic {
-    return this._italic;
+    return _italic;
   }
 
-  ///
   ///Set `Italic`
-  ///
   set isItalic(bool italic) {
-    this._italic = italic ?? false;
+    _italic = italic ?? false;
   }
 
   @override
-  bool operator ==(o) {
-    return o.isBold == this.isBold && // bold
-        o.rotation == this.rotation && // rotation
-        o.isItalic == this.isItalic && // italic
-        o.fontSize == this.fontSize && // Font Size
-        o.fontFamily == this.fontFamily &&
-        o.runtimeType == this.runtimeType && // runtimeType
-        o.wrap == this.wrap && // Font Wrapping
-        o.fontColor == this.fontColor && // Font Color
-        o.verticalAlignment == this.verticalAlignment && // Vertical Align
-        o.horizontalAlignment == this.horizontalAlignment && // Horizontal Align
-        o.backgroundColor == this.backgroundColor; // Background Color
-  }
-
-  @override
-  String toString() {
-    String b = "Background Color: " + this._backgroundColorHex;
-    String f = "Font Color: " + this._fontColorHex;
-    return b + "\n" + f;
-  }
+  List<Object> get props => [
+        _bold,
+        _rotation,
+        _italic,
+        _underline,
+        _fontSize,
+        _fontFamily,
+        _textWrapping,
+        _verticalAlign,
+        _horizontalAlign,
+        _fontColorHex,
+        _backgroundColorHex,
+      ];
 }
