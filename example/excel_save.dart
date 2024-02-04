4ec30e7ddabe6939a0673e340c25417673ee7c70
devsdocs <id45.go@gmail.com>
2024-02-05 00:09:19 +0900

    Update example

--- example/excel_save.dart
+++ example/excel_save.dart
@@ -5,6 +5,7 @@ import 'package:excel/excel.dart';
 void main() {
   final excel = Excel.createExcel();
   int count = 0;
+  const testSheet = 'Sheet To Remove';
   (List<List<dynamic>>.generate(3, (_) => List<int>.generate(3, (i) => i + 1))
         ..insert(0, [
           'A',
@@ -15,7 +16,16 @@ void main() {
     excel.insertRowIterables(
         'Test Sheet',
         el.map((e) {
-          var string = e.toString();
+          final string = e.toString();
+          return int.tryParse(string) != null
+              ? IntCellValue(int.parse(string))
+              : TextCellValue(string);
+        }).toList(),
+        count);
+    excel.insertRowIterables(
+        testSheet,
+        el.map((e) {
+          final string = e.toString();
           return int.tryParse(string) != null
               ? IntCellValue(int.parse(string))
               : TextCellValue(string);
@@ -23,7 +33,9 @@ void main() {
         count);
     count++;
   });
+
   excel.delete(excel.getDefaultSheet()!);
+  excel.delete(testSheet);
   excel.rename('Test Sheet', 'Test Sheet Rename');
   excel.setDefaultSheet('Test Sheet Rename');
 
